3d4fed89896b4948572801adf27472e8ccef8e9c
Hendrik Lind <hendrik@sshcrack.me>
2025-12-30 14:40:02 +0100

    impove tests

--- no_unqualified_libobs_uses/ui/main.rs
+++ no_unqualified_libobs_uses/ui/main.rs
@@ -1,6 +1,8 @@
 // This test should trigger the lint warning
 use libobs::obs_get_audio;
 
+use libobs_window_helper::{WindowSearchMode, get_all_windows};
+
 fn test_unqualified() {
     unsafe { obs_get_audio() };
 }
@@ -9,5 +11,7 @@ fn main() {}
 
 // This test should NOT trigger the lint warning
 fn test_qualified() {
+    // This is from the libobs_sys crate, so it should work fine
+    let _ = get_all_windows(WindowSearchMode::ExcludeMinimized);
     unsafe { libobs::obs_get_audio() };
 }
